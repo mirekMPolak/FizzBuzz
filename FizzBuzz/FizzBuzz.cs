ad1965c91e24fe04e4ed9f01d6bb3d0c5678d7a2
Mirek Polak <mirek.polak@o2.pl>
2024-01-16 10:50:23 +0100

    Add project files.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzz
+    {
+        private readonly long _numberLong;
+
+        public FizzBuzz(long numberLong)
+        {
+            _numberLong = numberLong;
+        }
+
+        public string IsDivisible()
+        {
+            var answerIsDivisibleByThree = String.Empty;
+            var answerIsDivisibleByFive = String.Empty;
+            string answerIsDivisibleByBoth;
+
+            if (_numberLong % 3 == 0)
+            {
+                answerIsDivisibleByThree = "Fizz";
+            }
+
+            if (_numberLong % 5 == 0)
+            {
+                answerIsDivisibleByFive = "Buzz";
+            }
+
+            answerIsDivisibleByBoth = answerIsDivisibleByThree + answerIsDivisibleByFive;
+
+            if (string.IsNullOrWhiteSpace(answerIsDivisibleByBoth))
+            {
+                answerIsDivisibleByBoth = _numberLong.ToString();
+            }
+            return answerIsDivisibleByBoth;
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"Podaj dowolną liczbę całkowitą z przedziału: {long.MinValue + 1}...-3, -2, -1, 0, 1, 2, 3,...{long.MaxValue - 1} :");
+
+                var number = CheckInt();
+
+                FizzBuzz playFizzBuzz = new FizzBuzz(number);
+
+                Console.WriteLine(playFizzBuzz.IsDivisible());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+
+        private static long CheckInt()
+        {
+            var input = IsNotEmpty();
+
+            var permissibleValuesForInputInt = "-1234567890";
+
+            foreach (var sign in input)
+            {
+                if (!permissibleValuesForInputInt.Contains(sign))
+                {
+                    throw new Exception("Wprowadzane dane muszą składać się wyłącznie z cyfr oraz ewentualnie znaku '-' na początku.!");
+                }
+            }
+
+            if (input.IndexOf("-", 0, input.Length, StringComparison.CurrentCulture) > 0 | input == "-")
+            {
+                throw new Exception("Znak minus tylko na początku, przed liczbą.!");
+            }
+
+            if (!long.TryParse(input, out long inputInt))
+            {
+                throw new Exception("Podana wartość jest nieprawidłowa ( z poza podanego zakresu ).!");
+            }
+            return inputInt;
+        }
+
+        private static string IsNotEmpty()
+        {
+            var input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("Nie wypełniono pola.!");
+            }
+            return input;
+        }
+    }
+}
